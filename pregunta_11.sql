57cedfec6938a9bcc9bb22fdf396ac8bac53104a
KeylaGarciaJaimes <kjgarciaj@unal.edu.co>
2022-04-30 07:49:36 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,7 +38,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT strftime('%Y', c14),COUNT(*) AS 'CANT'
-FROM tbl1\
-WHERE strftime('%Y', c14) = '2018'
-    
+    SELECT strftime('%Y', c14) AS YEAR, COUNT(*) AS CANT
+    FROM tbl1
+    WHERE strftime('%Y', c14) = '2018'
